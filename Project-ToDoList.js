adeb96e920621dcd368efbf61e1bd59b715e27bc
Hammad Imtiaz <imtiazhammad12@gmail.com>
2018-10-17 22:43:59 +0500

    Add files via upload

---
+++ Project-ToDoList.js
@@ -0,0 +1,20 @@
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+$("ul").on("click", "span", function(event){
+  event.stopPropagation();
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+});
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    var todoText = $(this).val();
+    $(this).val("");
+
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span></span> " + todoText + "</li>");
+  }
+});
+$(".fa-plus").click(function(){
+  $("input[type='text']").fadeToggle();
+});
